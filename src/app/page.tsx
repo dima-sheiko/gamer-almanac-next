7541566ef3bdf31f632a41dcf9f36eae614d9854
Dima <sheydim@gmail.com>
2023-05-29 01:12:20 +0300

    feat: add search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Search from '../components/search';
+
 export default function Home() {
-  return <main>App</main>;
+  return (
+    <main className="mt-10 flex flex-col items-center justify-start gap-10">
+      <Search />
+    </main>
+  );
 }
