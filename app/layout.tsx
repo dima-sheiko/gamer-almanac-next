909fdbe579c0e03b24dc3b07ea94c7e438e758f6
Dima <sheydim@gmail.com>
2023-05-28 16:35:39 +0300

    feat: add default font & container

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import Navbar from './components/navbar';
+import { Roboto_Flex } from 'next/font/google';
+import Header from './components/header';
 import './styles/globals.css';
 
 export const metadata = {
@@ -6,16 +7,26 @@ export const metadata = {
   description: 'Gaming library app',
 };
 
+const roboto = Roboto_Flex({
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        {children}
+    <html
+      lang="en"
+      className={roboto.className}
+    >
+      <body className="bg-emerald-100">
+        <div className="m-auto max-w-[1440px] p-8">
+          <Header />
+          <main>{children}</main>
+        </div>
       </body>
     </html>
   );
