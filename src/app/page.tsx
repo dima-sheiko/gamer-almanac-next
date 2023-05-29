da8ee258025ac62eae9e7bd27d1a567f1c05e7fb
Dima <sheydim@gmail.com>
2023-05-29 19:44:42 +0300

    feat: add card container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import CardContainer from '../components/cards/cardContainer';
 import Settings from '../components/settings';
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className="mt-10 flex flex-col items-center justify-start gap-10">
       <Settings />
+      <CardContainer />
     </main>
   );
 }
