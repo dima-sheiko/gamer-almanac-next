abc47bd900dda48c8c7cd7fbebef449530490e95
Dima <sheydim@gmail.com>
2023-05-29 13:40:22 +0300

    feat: add custom properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'select-arrow':
+          "url(\"data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e\")",
+      },
+      backgroundSize: {
+        em: '1em',
       },
     },
   },
   plugins: [],
-}
+};
