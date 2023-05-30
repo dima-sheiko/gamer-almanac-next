72c7c0ae110e2a09f049a114c2f264e1ae8cd321
Dima <sheydim@gmail.com>
2023-05-30 11:48:37 +0300

    feat: pass filter and sort params

--- src/lib/getGamesData.ts
+++ src/lib/getGamesData.ts
@@ -1,15 +1,19 @@
+import { FilterParams, SortParams } from '../constants/enums';
 import { GamesData } from './types';
 
-async function getGamesData() {
-  const res = await fetch('https://637bace46f4024eac21566d8.mockapi.io/games');
+async function getGamesData(
+  filterParam: FilterParams,
+  sortParam: SortParams,
+): Promise<GamesData[]> {
+  const res = await fetch(
+    `https://637bace46f4024eac21566d8.mockapi.io/games?&genre=${filterParam}&sortBy=${sortParam}`,
+  );
 
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
 
-  const games = (await res.json()) as GamesData[];
-
-  return games;
+  return res.json();
 }
 
 export default getGamesData;
