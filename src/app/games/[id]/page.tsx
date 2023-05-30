e85dfd725d0af812a1e0f05924d51df892d9775b
Dima <sheydim@gmail.com>
2023-05-31 00:10:34 +0300

    feat: create dynamic routes for each game

--- src/app/games/[id]/page.tsx
+++ src/app/games/[id]/page.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import getGamesData from '@/src/lib/getGamesData';
 
-export default function GamePage() {
-  return (
-    <div>GamePage</div>
-  );
+interface Params {
+  params: {
+    id: string;
+  };
+}
+
+export async function generateStaticParams() {
+  const games = await getGamesData();
+
+  return games.map((game) => ({
+    id: game.id,
+  }));
+}
+
+export async function generateMetadata({ params }: Params) {
+  const games = await getGamesData();
+  const { id } = params;
+
+  const game = games.find((x) => x.id === id);
+
+  if (!game) {
+    return {
+      title: 'Game Not Found',
+    };
+  }
+
+  return {
+    title: game.title,
+  };
+}
+
+export default function GamePage({ params }: Params) {
+  return <div>{params.id}</div>;
 }
