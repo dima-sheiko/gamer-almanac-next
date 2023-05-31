1e5213656c3e17036de0536c7931b0ab98ed2bc9
Dima <sheydim@gmail.com>
2023-05-31 19:45:24 +0300

    feat: add link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { Silkscreen } from 'next/font/google';
+import Link from 'next/link';
 
 const silkscreen = Silkscreen({
   subsets: ['latin'],
@@ -9,10 +10,12 @@ const silkscreen = Silkscreen({
 export default function Header() {
   return (
     <header className={silkscreen.className}>
-      <h1 className="text-center text-5xl sm:text-6xl">
-        Gamer Almanac
-        <sub className="align-top text-xl sm:text-2xl">2.0</sub>
-      </h1>
+      <Link href="/">
+        <h1 className="text-center text-5xl sm:text-6xl">
+          Gamer Almanac
+          <sub className="align-top text-xl sm:text-2xl">2.0</sub>
+        </h1>
+      </Link>
     </header>
   );
 }
