0374b337b67a9969eec7341a12131f69eb584d0c
Dima <sheydim@gmail.com>
2023-05-31 19:46:40 +0300

    feat: add 404 component

--- src/components/cards/cardContainer.tsx
+++ src/components/cards/cardContainer.tsx
@@ -5,6 +5,7 @@ import { useFilter, useSearch, useSort } from '@/src/store';
 import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 import Card from './card';
+import NotFound from '../404';
 
 export default function CardContainer() {
   const searchQuery = useSearch((state) => state.searchQuery);
@@ -41,18 +42,7 @@ export default function CardContainer() {
   }
 
   if (!content?.length) {
-    return (
-      <div className="flex flex-col items-center gap-2 text-center text-xl">
-        <p>It&apos;s empty here.</p>
-        <p>But here&apos;s a cute little bird instead!</p>
-        <Image
-          src="/bird.svg"
-          alt="no data"
-          width={40}
-          height={40}
-        />
-      </div>
-    );
+    return <NotFound />;
   }
 
   return (
