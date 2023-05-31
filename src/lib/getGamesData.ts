afeef65cfd2090968cb5fd88eb5e5f19fbd2c9f5
Dima <sheydim@gmail.com>
2023-05-31 22:10:53 +0300

    feat: add revalidation param

--- src/lib/getGamesData.ts
+++ src/lib/getGamesData.ts
@@ -1,9 +1,9 @@
 import { GamesData } from './types';
 
 async function getGamesData(): Promise<GamesData[]> {
-  const res = await fetch(
-    'https://637bace46f4024eac21566d8.mockapi.io/games',
-  );
+  const res = await fetch('https://637bace46f4024eac21566d8.mockapi.io/games', {
+    next: { revalidate: 60 },
+  });
 
   if (!res.ok) {
     throw new Error('Failed to fetch data');
