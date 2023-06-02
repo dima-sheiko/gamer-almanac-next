67cfc4df531bfae19d8a62619991314bc829e50b
Dima <sheydim@gmail.com>
2023-06-02 15:02:42 +0300

    fix: image styling

--- src/components/cards/card.tsx
+++ src/components/cards/card.tsx
@@ -12,14 +12,12 @@ export default function Card({ game }: ICardProps) {
     <Link href={`/games/${game.id}`}>
       <div className="relative flex flex-col rounded-md bg-white text-slate-800 shadow-md duration-500 hover:scale-105 dark:bg-slate-300">
         <Image
-          className="rounded-t-md"
+          className="w-full rounded-t-md"
           src={game.header}
           alt="game artwork"
           width={0}
-          height={0}
+          height={186.95}
           sizes="100vw"
-          priority={game.id === '19'}
-          style={{ width: '100%', height: 'auto' }}
         />
         <div className="flex flex-col justify-start gap-2 p-5">
           <h3 className="text-lg font-medium">{game.title}</h3>
