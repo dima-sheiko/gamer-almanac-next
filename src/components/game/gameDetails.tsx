e3d3c545a6d1e7fea786bfd9ead7c4fd21367378
Dima <sheydim@gmail.com>
2023-06-02 14:56:42 +0300

    fix: add atrribute & refactor button

--- src/components/game/gameDetails.tsx
+++ src/components/game/gameDetails.tsx
@@ -11,19 +11,20 @@ export default function GameDetails({ game }: IGameDetailsProps) {
       <h2 className="text-center text-2xl font-bold underline xl:text-start">
         {game.title}
       </h2>
-      <p className="text-center xl:text-start dark:text-slate-300">{game.description}</p>
+      <p className="text-center dark:text-slate-300 xl:text-start">
+        {game.description}
+      </p>
       <a
         href={game.link}
         target="_blank"
-        className="w-full max-w-[448px] self-center rounded-md bg-emerald-300 px-5 py-3 text-center text-slate-800 font-medium shadow-md hover:bg-emerald-400 active:bg-emerald-500 xl:max-w-none"
+        rel="noreferrer"
+        className="w-full flex justify-center items-center gap-1 max-w-[448px] self-center rounded-md bg-emerald-300 px-5 py-3 text-center font-medium text-slate-800 shadow-md hover:bg-emerald-400 active:bg-emerald-500 xl:max-w-none"
       >
-        <div className="flex justify-center gap-2">
-          <p>Get It on Steam</p>
-          <Icon
-            width="15"
-            href="/icons/sprites/utils-sprite.svg#external-link"
-          />
-        </div>
+        <span>Get It on Steam</span>
+        <Icon
+          width="15"
+          href="/icons/sprites/utils-sprite.svg#external-link"
+        />
       </a>
     </div>
   );
